8c584468fc52ba3d707d1af51ba0a31fe9563414
Gareth <gareth.w.owen@gmail.com>
2020-08-10 21:31:50 +0100

    get basic window showing

--- src/random_programs/texture_map.rs
+++ src/random_programs/texture_map.rs
@@ -1,3 +1,115 @@
+use vk_playground::{
+    utility,
+    utility::constants::*,
+    utility::debug::*,
+    utility::share,
+};
+
+use ash::version::DeviceV1_0;
+use ash::version::InstanceV1_0;
+use ash::vk;
+use winit::event::{Event, VirtualKeyCode, ElementState, KeyboardInput, WindowEvent};
+use winit::event_loop::{EventLoop, ControlFlow};
+
+use std::ptr;
+
+
+// constants
+const WINDOW_TITLE: &'static str = "Basic Window";
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
+struct VulkanApp {
+    window: winit::window::Window,
+
+    _entry: ash::Entry,
+    instance: ash::Instance,
+    surface_loader: ash::extensions::khr::Surface,
+    surface: vk::SurfaceKHR,
+    debug_utils_loader: ash::extensions::ext::DebugUtils,
+    debug_messenger: vk::DebugUtilsMessengerEXT,
+
+    _physical_device: vk::PhysicalDevice,
+    device: ash::Device,
+
+    graphics_queue: vk::Queue,
+    present_queue: vk::Queue,
+
+    swapchain_loader: ash::extensions::khr::Swapchain,
+    swapchain: vk::SwapchainKHR,
+    _swapchain_images: Vec<vk::Image>,
+    _swapchain_format: vk::Format,
+    _swapchain_extent: vk::Extent2D,
+    swapchain_imageviews: Vec<vk::ImageView>,
+    swapchain_framebuffers: Vec<vk::Framebuffer>,
+
+    render_pass: vk::RenderPass,
+    pipeline_layout: vk::PipelineLayout,
+    graphics_pipeline: vk::Pipeline,
+
+    command_pool: vk::CommandPool,
+    command_buffers: Vec<vk::CommandBuffer>,
+
+    image_available_semaphores: Vec<vk::Semaphore>,
+    render_finished_sempahores: Vec<vk::Semaphore>,
+    in_flight_fences: Vec<vk::Fence>,
+    current_frame: usize,
+}
+
+impl VulkanApp {
+    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
+        winit::window::WindowBuilder::new() 
+        .with_title(WINDOW_TITLE)
+        .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH,WINDOW_HEIGHT))
+        .build(event_loop)
+        .expect("Failed to create window!")
+    }
+
+    pub fn main_loop(event_loop: EventLoop<()>) {
+
+        event_loop.run(move |event, _, control_flow| {
+
+            match event {
+                // handle window events
+                | Event::WindowEvent { event, .. } => {
+                    // handle closing
+                    match event {
+                        | WindowEvent::CloseRequested => {
+                            *control_flow = ControlFlow::Exit
+                        },
+                        // handle keyboard input
+                        | WindowEvent::KeyboardInput { input, .. } => {
+                            // handle different key cases
+                            match input {
+                                // various keys from the keyboard
+                                | KeyboardInput { virtual_keycode, state, .. } => {
+                                    // someone's pressed escape!
+                                    match (virtual_keycode, state) {
+                                        | (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
+                                            dbg!();
+                                            *control_flow = ControlFlow::Exit
+                                        },
+                                        | _ => {},
+                                    }
+                                },
+                            }
+                            // end of handling key cases
+                        }, // end of handling window events
+                        | _ => {},
+                    }
+                },
+                _ => (),
+            }
+
+        })
+    }
+
+}
+
 fn main() {
-    println!("plllaaaaaaaaaaccccccccehhhhhhhooolder!");
+
+    let event_loop = EventLoop::new();    
+    let _window = VulkanApp::init_window(&event_loop);
+
+    VulkanApp::main_loop(event_loop);
 }
