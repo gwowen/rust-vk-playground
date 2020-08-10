f5666d0e696fd1a84a1dc9d934a34510a1b63223
Gareth <gareth.w.owen@gmail.com>
2020-08-10 22:56:04 +0100

    add VertexV2, descriptors stuff and texture

--- src/random_programs/texture_map.rs
+++ src/random_programs/texture_map.rs
@@ -3,23 +3,70 @@ use vk_playground::{
     utility::constants::*,
     utility::debug::*,
     utility::share,
+    utility::structures::*,
+    utility::window::{ProgramProc,},
 };
 
 use ash::version::DeviceV1_0;
 use ash::version::InstanceV1_0;
 use ash::vk;
-use winit::event::{Event, VirtualKeyCode, ElementState, KeyboardInput, WindowEvent};
-use winit::event_loop::{EventLoop, ControlFlow};
+use cgmath::{Deg, Matrix4, Point3, Vector3};
+use memoffset::offset_of;
 
+
+use std::ffi::CString;
+use std::path::Path;
 use std::ptr;
 
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub struct VertexV2 {
+    pub pos: [f32; 2],
+    pub color: [f32; 3],
+    pub tex_coord: [f32; 2],
+}
+
+impl VertexV2 {
+    pub fn get_binding_description() -> [vk::VertexInputBindingDescription; 1] {
+        [vk::VertexInputBindingDescription {
+            binding: 0,
+            stride: std::mem::size_of::<VertexV2>() as u32,
+            input_rate: vk::VertexInputRate::VERTEX,
+        }]
+    }
+
+    pub fn get_attribute_descriptions() -> [vk::VertexInputAttributeDescription; 3] {
+        [
+            vk::VertexInputAttributeDescription {
+                binding: 0,
+                location: 0, 
+                format: vk::Format::R32G32_SFLOAT,
+                offset: offset_of!(VertexV2, pos) as u32,
+            },
+
+            vk::VertexInputAttributeDescription {
+                binding: 0,
+                location: 1, 
+                format: vk::Format::R32G32B32_SFLOAT,
+                offset: offset_of!(VertexV2, color) as u32,
+            },
+
+            vk::VertexInputAttributeDescription {
+                binding: 0,
+                location: 2, 
+                format: vk::Format::R32G32B32_SFLOAT,
+                offset: offset_of!(VertexV2, tex_coord) as u32,
+            },
+        ]
+    }
+}
 
 // constants
-const WINDOW_TITLE: &'static str = "Basic Window";
+const WINDOW_TITLE: &'static str = "Texture Mapping!";
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
 
-struct VulkanApp {
+struct VulkanAppTextureMapping {
     window: winit::window::Window,
 
     _entry: ash::Entry,
@@ -29,23 +76,42 @@ struct VulkanApp {
     debug_utils_loader: ash::extensions::ext::DebugUtils,
     debug_messenger: vk::DebugUtilsMessengerEXT,
 
-    _physical_device: vk::PhysicalDevice,
+    physical_device: vk::PhysicalDevice,
     device: ash::Device,
 
+    queue_family: QueueFamilyIndices,
     graphics_queue: vk::Queue,
     present_queue: vk::Queue,
 
     swapchain_loader: ash::extensions::khr::Swapchain,
     swapchain: vk::SwapchainKHR,
-    _swapchain_images: Vec<vk::Image>,
-    _swapchain_format: vk::Format,
-    _swapchain_extent: vk::Extent2D,
+    swapchain_images: Vec<vk::Image>,
+    swapchain_format: vk::Format,
+    swapchain_extent: vk::Extent2D,
     swapchain_imageviews: Vec<vk::ImageView>,
     swapchain_framebuffers: Vec<vk::Framebuffer>,
 
     render_pass: vk::RenderPass,
+    ubo_layout: vk::DescriptorSetLayout,
     pipeline_layout: vk::PipelineLayout,
     graphics_pipeline: vk::Pipeline,
+
+    texture_image: vk::Image,
+    texture_image_view: vk::ImageView,
+    texture_sampler: vk::Sampler,
+    texture_image_memory: vk::DeviceMemory,
+
+    vertex_buffer: vk::Buffer,
+    vertex_buffer_memory: vk::DeviceMemory,
+    index_buffer: vk::Buffer,
+    index_buffer_memory: vk::DeviceMemory,
+
+    uniform_transform: UniformBufferObject,
+    uniform_buffers: Vec<vk::Buffer>,
+    uniform_buffers_memory: Vec<vk::DeviceMemory>,
+
+    desciptor_pool: vk::DescriptorPool,
+    descriptor_sets: Vec<vk::DescriptorSet>,
 
     command_pool: vk::CommandPool,
     command_buffers: Vec<vk::CommandBuffer>,
@@ -54,62 +120,26 @@ struct VulkanApp {
     render_finished_sempahores: Vec<vk::Semaphore>,
     in_flight_fences: Vec<vk::Fence>,
     current_frame: usize,
+
+    is_framebuffer_resized: bool,
 }
 
-impl VulkanApp {
-    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
-        winit::window::WindowBuilder::new() 
-        .with_title(WINDOW_TITLE)
-        .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH,WINDOW_HEIGHT))
-        .build(event_loop)
-        .expect("Failed to create window!")
-    }
-
-    pub fn main_loop(event_loop: EventLoop<()>) {
-
-        event_loop.run(move |event, _, control_flow| {
-
-            match event {
-                // handle window events
-                | Event::WindowEvent { event, .. } => {
-                    // handle closing
-                    match event {
-                        | WindowEvent::CloseRequested => {
-                            *control_flow = ControlFlow::Exit
-                        },
-                        // handle keyboard input
-                        | WindowEvent::KeyboardInput { input, .. } => {
-                            // handle different key cases
-                            match input {
-                                // various keys from the keyboard
-                                | KeyboardInput { virtual_keycode, state, .. } => {
-                                    // someone's pressed escape!
-                                    match (virtual_keycode, state) {
-                                        | (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
-                                            dbg!();
-                                            *control_flow = ControlFlow::Exit
-                                        },
-                                        | _ => {},
-                                    }
-                                },
-                            }
-                            // end of handling key cases
-                        }, // end of handling window events
-                        | _ => {},
-                    }
-                },
-                _ => (),
+impl VulkanAppTextureMapping {
+    pub fn new(event_loop: &winit::event_loop::EventLoop<()>) -> VulkanAppTextureMapping {
+       let window =
+            utility::window::init_window(&event_loop, WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT);
+            
+            VulkanAppTextureMapping {
+                window,
             }
-
-        })
     }
 
 }
 
 fn main() {
 
-    let event_loop = EventLoop::new();    
-    let _window = VulkanApp::init_window(&event_loop);
+    let program_proc = ProgramProc::new();   
+    let vulkan_app = VulkanAppTextureMapping::new(&program_proc.event_loop);
 
-    VulkanApp::main_loop(event_loop);
+    program_proc.main_loop(vulkan_app);
 }
