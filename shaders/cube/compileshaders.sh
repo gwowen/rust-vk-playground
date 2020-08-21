95681ae07a0cdbec9c98ce5c3c9cd7345e4ab6fe
Gareth <gareth.w.owen@gmail.com>
2020-08-21 02:03:18 +0100

    add shaders, something funky be up with renderpass

---
+++ shaders/cube/compileshaders.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+glslangValidator -V cube.frag
+glslangValidator -V cube.vert
--- src/random_programs/cube.rs
+++ src/random_programs/cube.rs
@@ -341,8 +341,12 @@ impl VulkanAppCube {
             swapchain_stuff.swapchain_format,
             &swapchain_stuff.swapchain_images,
         );
-        let render_pass = share::v1::create_render_pass(&device, swapchain_stuff.swapchain_format);
-        let ubo_layout = VulkanAppCube::create_descriptor_set_layout(&device);
+        let render_pass = VulkanAppCube::create_render_pass(
+            &instance,
+            &device, 
+            physical_device,
+            swapchain_stuff.swapchain_format);
+        let ubo_layout = share::v2::create_descriptor_set_layout(&device);
         let (graphics_pipeline, pipeline_layout) = VulkanAppCube::create_graphics_pipeline(
             &device,
             render_pass,
@@ -395,8 +399,8 @@ impl VulkanAppCube {
             swapchain_stuff.swapchain_images.len()
         );
         let descriptor_pool =
-            VulkanAppCube::create_descriptor_pool(&device, swapchain_stuff.swapchain_images.len());
-        let descriptor_sets = VulkanAppCube::create_descriptor_sets(
+            share::v2::create_descriptor_pool(&device, swapchain_stuff.swapchain_images.len());
+        let descriptor_sets = share::v2::create_descriptor_sets(
             &device,
             descriptor_pool,
             ubo_layout,
@@ -582,7 +586,6 @@ impl VulkanAppCube {
     fn has_stencil_component(format: vk::Format) -> bool {
         format == vk::Format::D32_SFLOAT_S8_UINT || format == vk::Format::D24_UNORM_S8_UINT 
     }
-
     // end of depth buffer functions
 
     fn create_render_pass(
@@ -714,11 +717,11 @@ impl VulkanAppCube {
     ) -> (vk::Pipeline, vk::PipelineLayout) {
         let vert_shader_module = share::create_shader_module(
             device,
-            "shaders/texture_map/vert.spv",
+            "shaders/cube/vert.spv",
         );
         let frag_shader_module = share::create_shader_module(
             device,
-            "shaders/texture_map/frag.spv",
+            "shaders/cube/frag.spv",
         );
 
         let main_function_name = CString::new("main").unwrap(); // the beginning function name in shader code.
@@ -923,152 +926,9 @@ impl VulkanAppCube {
         (graphics_pipelines[0], pipeline_layout)
     }
 
-    fn create_descriptor_pool (
-        device: &ash::Device,
-        swapchain_images_size: usize,
-    ) -> vk::DescriptorPool {
-        let pool_sizes = [
-            vk::DescriptorPoolSize {
-                // transform descriptor pool
-                ty: vk::DescriptorType::UNIFORM_BUFFER,
-                descriptor_count: swapchain_images_size as u32,
-            },
-            vk::DescriptorPoolSize {
-                // sampler descriptor pool
-                ty: vk::DescriptorType::COMBINED_IMAGE_SAMPLER,
-                descriptor_count: swapchain_images_size as u32,
-            },
-        ];
+    
 
-        let descriptor_pool_create_info = vk::DescriptorPoolCreateInfo {
-            s_type: vk::StructureType::DESCRIPTOR_POOL_CREATE_INFO,
-            p_next: ptr::null(),
-            flags: vk::DescriptorPoolCreateFlags::empty(),
-            max_sets: swapchain_images_size as u32,
-            pool_size_count: pool_sizes.len() as u32,
-            p_pool_sizes: pool_sizes.as_ptr(),
-        };
-
-        unsafe {
-            device
-                .create_descriptor_pool(&descriptor_pool_create_info, None)
-                .expect("Failed to create descriptor pool!")
-        }
-    }
-
-    fn create_descriptor_sets(
-        device: &ash::Device,
-        descriptor_pool: vk::DescriptorPool,
-        descriptor_set_layout: vk::DescriptorSetLayout,
-        uniforms_buffers: &Vec<vk::Buffer>,
-        texture_image_view: vk::ImageView,
-        texture_sampler: vk::Sampler,
-        swapchain_images_size: usize,
-    ) -> Vec<vk::DescriptorSet> {
-        let mut layouts: Vec<vk::DescriptorSetLayout> = vec![];
-
-        for _ in 0..swapchain_images_size {
-            layouts.push(descriptor_set_layout);
-        }
-
-        let descriptor_set_allocate_info = vk::DescriptorSetAllocateInfo {
-            s_type: vk::StructureType::DESCRIPTOR_SET_ALLOCATE_INFO,
-            p_next: ptr::null(),
-            descriptor_pool,
-            descriptor_set_count: swapchain_images_size as u32,
-            p_set_layouts: layouts.as_ptr(),
-        };
-
-        let descriptor_sets = unsafe {
-            device
-                .allocate_descriptor_sets(&descriptor_set_allocate_info)
-                .expect("Failed to allocate descriptor sets!")
-        };
-
-        for (i, &descritptor_set) in descriptor_sets.iter().enumerate() {
-            let descriptor_buffer_infos = [vk::DescriptorBufferInfo {
-                buffer: uniforms_buffers[i],
-                offset: 0,
-                range: ::std::mem::size_of::<UniformBufferObject>() as u64,
-            }];
-
-            let descriptor_image_infos = [vk::DescriptorImageInfo {
-                sampler: texture_sampler,
-                image_view: texture_image_view,
-                image_layout: vk::ImageLayout::SHADER_READ_ONLY_OPTIMAL,
-            }];
-
-            let descriptor_write_sets = [
-                vk::WriteDescriptorSet {
-                    // transform uniform
-                    s_type: vk::StructureType::WRITE_DESCRIPTOR_SET,
-                    p_next: ptr::null(),
-                    dst_set: descritptor_set,
-                    dst_binding: 0,
-                    dst_array_element: 0,
-                    descriptor_count: 1,
-                    descriptor_type: vk::DescriptorType::UNIFORM_BUFFER,
-                    p_image_info: ptr::null(),
-                    p_buffer_info: descriptor_buffer_infos.as_ptr(),
-                    p_texel_buffer_view: ptr::null(),
-                },
-                vk::WriteDescriptorSet {
-                    // sampler uniform
-                    s_type: vk::StructureType::WRITE_DESCRIPTOR_SET,
-                    p_next: ptr::null(),
-                    dst_set: descritptor_set,
-                    dst_binding: 1,
-                    dst_array_element: 0,
-                    descriptor_count: 1,
-                    descriptor_type: vk::DescriptorType::COMBINED_IMAGE_SAMPLER,
-                    p_image_info: descriptor_image_infos.as_ptr(),
-                    p_buffer_info: ptr::null(),
-                    p_texel_buffer_view: ptr::null(),
-                },
-            ];
-
-            unsafe {
-                device.update_descriptor_sets(&descriptor_write_sets, &[]);
-            }
-        }
-
-        descriptor_sets
-    }
-
-    fn create_descriptor_set_layout(device: &ash::Device) -> vk::DescriptorSetLayout {
-        let ubo_layout_bindings = [
-            vk::DescriptorSetLayoutBinding {
-                // transform uniform
-                binding: 0,
-                descriptor_type: vk::DescriptorType::UNIFORM_BUFFER,
-                descriptor_count: 1,
-                stage_flags: vk::ShaderStageFlags::VERTEX,
-                p_immutable_samplers: ptr::null(),
-            },
-            vk::DescriptorSetLayoutBinding {
-                //sampler uniform
-                binding: 1,
-                descriptor_type: vk::DescriptorType::COMBINED_IMAGE_SAMPLER,
-                descriptor_count: 1, 
-                stage_flags: vk::ShaderStageFlags::FRAGMENT,
-                p_immutable_samplers: ptr::null(),
-            }
-        ];
-
-        let ubo_layout_create_info = vk::DescriptorSetLayoutCreateInfo {
-            s_type: vk::StructureType::DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
-            p_next: ptr::null(),
-            flags: vk::DescriptorSetLayoutCreateFlags::empty(),
-            binding_count: ubo_layout_bindings.len() as u32,
-            p_bindings: ubo_layout_bindings.as_ptr(),
-        };
-
-        unsafe {
-            device
-                .create_descriptor_set_layout(&ubo_layout_create_info, None)
-                .expect("Failed to create descriptor set layout")
-        }
-    }
+   
 }
 
 impl VulkanAppCube {
