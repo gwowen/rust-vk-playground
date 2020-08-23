d7d5fdde45a1f82d5e6fdae7153d9bfc1a4f6501
Gareth <gareth.w.owen@gmail.com>
2020-08-23 23:50:23 +0100

    more fun with typos

--- src/utility/platforms.rs
+++ src/utility/platforms.rs
@@ -3,12 +3,8 @@ use ash::vk;
 
 #[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
 use ash::extensions::khr::XlibSurface;
-// this seems to fail to compile on Linux...
-// guess it's not surprising but still would
-// like to leave it uncommented and let the compiler
-// figure it out...
-// #[cfg(target_os = "windows")]
-// use ash::extensions::khr::Win32Surface;
+#[cfg(target_os = "windows")]
+use ash::extensions::khr::Win32Surface;
 
 use ash::extensions::ext::DebugUtils;
 use ash::extensions::khr::Surface;
@@ -44,28 +40,28 @@ pub unsafe fn create_surface<E: EntryV1_0, I: InstanceV1_0>(
     xlib_surface_loader.create_xlib_surface(&x11_create_info, None)
 }
 
-// #[cfg(target os = "windows")]
-// pub unsafe fn create_surface<E: EntryV1_0, I: InstanceV1_0>(
-//     entry: &E,
-//     instance: &I,
-//     window: &winit::window::Window,
-// ) -> Result<vk::SurfaceKHR, vk::Result> {
-//     use std::os::raw::c_void;
-//     use std::ptr;
-//     use winapi::shared::windef::HWND;
-//     use winapi::um::libloaderapi::GetModuleHandleW;
-//     use winit::platform::windows::WindowExtWindows;
+#[cfg(target_os = "windows")]
+pub unsafe fn create_surface<E: EntryV1_0, I: InstanceV1_0>(
+    entry: &E,
+    instance: &I,
+    window: &winit::window::Window,
+) -> Result<vk::SurfaceKHR, vk::Result> {
+    use std::os::raw::c_void;
+    use std::ptr;
+    use winapi::shared::windef::HWND;
+    use winapi::um::libloaderapi::GetModuleHandleW;
+    use winit::platform::windows::WindowExtWindows;
 
-//     let hwnd = window.hwnd() as HWND;
-//     let hinstance = GetModuleHandleW(ptr::null()) as *const c_void;
-//     let win32_create_info = vk::Win32SurfaceCreateInfoKHR {
-//         s_type: vk::StructureType::WIN32_SURFACE_CREATE_INFO_KHR,
-//         p_next: ptr::null(),
-//         flags: Default::default(),
-//         hinstance,
-//         hwnd: hwnd as *const c_void,
-//     };
-//     let win32_surface_loader = Win32Surface::new(entry, instance);
-//     win32_surface_loader.create_win32_surface(&win32_create_info, None)
-// }
+    let hwnd = window.hwnd() as HWND;
+    let hinstance = GetModuleHandleW(ptr::null()) as *const c_void;
+    let win32_create_info = vk::Win32SurfaceCreateInfoKHR {
+        s_type: vk::StructureType::WIN32_SURFACE_CREATE_INFO_KHR,
+        p_next: ptr::null(),
+        flags: Default::default(),
+        hinstance,
+        hwnd: hwnd as *const c_void,
+    };
+    let win32_surface_loader = Win32Surface::new(entry, instance);
+    win32_surface_loader.create_win32_surface(&win32_create_info, None)
+}
 
