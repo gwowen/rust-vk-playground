044750a1936a2c2ff80c440f1629d42d484eacfa
Gareth <gareth.w.owen@gmail.com>
2020-08-31 19:48:14 +0100

    move to vkstuff folder

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -7,14 +7,6 @@ pub mod debug;
 pub mod tools;
 pub mod structures;
 pub mod share;
-// new!
-pub mod descriptor_stuff;
-pub mod device_stuff;
-pub mod shader_stuff;
-pub mod buffer_stuff;
-pub mod image_stuff;
-pub mod swapchain_stuff;
-pub mod instance_stuff;
-pub mod format_stuff;
+
 
 
